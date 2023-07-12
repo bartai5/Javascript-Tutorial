52d3698609a9450365f35133b341997c1c075456
bartai5 <6mutaibartai58@gmail.com>
2023-07-12 13:38:05 +0300

    Updated the object section

--- object.js
+++ object.js
@@ -1,7 +1,9 @@
+// The car in this situation is an object
 let car = {
     make: "bmw",
     model: '745li',
     year: 2010,
+    // The functions below are 'methods'
     getPrice: function(){
         return 5000;
     },
@@ -9,7 +11,27 @@ let car = {
         console.log(this.make + " " + this.model);
     }
 }
+// car.printDescription();
+// console.log(car.year); // OR
+// // console.log(car['year']);
+// console.log(car.getPrice());
 
-car.printDescription();
-console.log(car.year);
-console.log(car.getPrice());
+// Object c
+let c = {
+    myProperty:[
+        {d: "This"},
+        {e: "is"},
+        {f: "an"},
+        {g: "array"},
+        {h: "object"}
+    ]
+}
+// Example 2
+let contact = {
+    customers: [
+        {firstName: "Japheth", lastName: "Kipkurui", contactNo: "+253727637947"},
+        {firstName: "Bartai", lastName: "Mutai", contactNo: "+254768302374"}
+    ]
+};
+// Finnaly 
+console.log(contact.customers[1].firstName);
