0fa80d9b557786a0e7553e2df22affbdb738f685
bartai5 <6mutaibartai58@gmail.com>
2023-07-13 17:05:32 +0300

    template-literals.js

---
+++ template-literals.js
@@ -0,0 +1,10 @@
+let names = "Japheth";
+console.log(`Hello ${names}`);
+
+// Intecting some code
+function getReasonCount(){
+    return 4; // or 2\ for the second argument
+}
+
+let concFunction = `Give me ${(getReasonCount() == 1 ? 'one good reason' : 'a lot of reasons')} to try this`;
+console.log(concFunction);
