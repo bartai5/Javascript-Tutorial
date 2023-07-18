33c9e6100239d95275fed3e4ecdaaf2f86b587cb
bartai5 <6mutaibartai58@gmail.com>
2023-07-18 10:21:05 +0300

    Uploaded the regex file

---
+++ regex.js
@@ -0,0 +1,6 @@
+let pattern = /japheth/;
+
+let value = "Hello, i am japheth and i am awesome!!!";
+console.log(pattern.test(value));
+let rep = value.replace(pattern, "Kipkurui");
+console.log(rep);
